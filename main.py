df2628cb5620acd1d52e199bca1ca4dd4404d8b8
danqunyu <danqunyu@163.com>
2022-07-04 00:30:05 +0800

    Main Form is created and demo show

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # project to develop LOGO UI at home
+import sys
+import hello
+from  PyQt5.QtWidgets import QApplication, QMainWindow
 
 if __name__ == '__main__':
+    app = QApplication(sys.argv)
     pass
+
+MainWindow = QMainWindow()
+ui = hello.Ui_Form()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
