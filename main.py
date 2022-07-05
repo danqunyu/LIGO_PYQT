b14c4290b6a2786d19957b8b655232162460a5b9
danqunyu <danqunyu@163.com>
2022-07-05 23:59:52 +0800

    finish job: select combo box item and list related arguments

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 # project to develop LOGO UI at home
 import sys
-import hello
-from  PyQt5.QtWidgets import QApplication, QMainWindow
+import LIGOPlatform
+# import hello
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+
+class MainForm(QMainWindow):
+    def __init__(self):
+        super(MainForm, self).__init__()
+        self.ui = LIGOPlatform.Ui_LIGOPlatform()
+        # self.ui.startUi()
+        self.ui.setupUi(self)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    pass
+    # mainWindow = MainForm()
+    # mainWindow.show()
 
-MainWindow = QMainWindow()
-ui = hello.Ui_Form()
-ui.setupUi(MainWindow)
-MainWindow.show()
-sys.exit(app.exec_())
+    mainwindow  = QMainWindow()
+    ui = LIGOPlatform.Ui_LIGOPlatform()
+    ui.startUi(mainwindow)
+    mainwindow.show()
+    sys.exit(app.exec_())
+
+
+    # MainWindow = QMainWindow()
+    # ui = hello.Ui_Form()
+    # ui.setupUi(MainWindow)
+    # MainWindow.show()
+    # sys.exit(app.exec_())
+    pass
